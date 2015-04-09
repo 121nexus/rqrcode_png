9e4303d74a48927db3ed404e081dbaa73c5c45f6
Joey Surface <jdtriska@gmail.com>
2015-04-09 14:30:19 -0400

    changed default background to transparent and allowed input of rgba colors

--- lib/rqrcode_png/image.rb
+++ lib/rqrcode_png/image.rb
@@ -9,10 +9,10 @@ module RQRCodePNG
     end
 
     # Returns an image file of the QR Code
-    def render bg_color = WHITE
+    def render (bg_color = TRANSPARENT, fg_color = BLACK)
       png = blank_img(bg_color)
       @sequence.dark_squares_only do |x, y|
-        png[y + @sequence.border_width(), x + @sequence.border_width()] = BLACK
+        png[y + @sequence.border_width(), x + @sequence.border_width()] = fg_color
       end
       return png
     end
@@ -24,8 +24,8 @@ module RQRCodePNG
       @img_size ||= @sequence.img_size() + @sequence.border_width() * 2
     end
 
-    # Returns an appropriately sized, blank (white) image
-    def blank_img bg_color = WHITE
+    # Returns an appropriately sized, blank (transparent) image
+    def blank_img bg_color = TRANSPARENT
       ::ChunkyPNG::Image.new(img_size(), img_size(), bg_color)
     end
   end
--- lib/rqrcode_png/qrcode_extensions.rb
+++ lib/rqrcode_png/qrcode_extensions.rb
@@ -1,8 +1,14 @@
 module RQRCodePNG
   module QRCodeExtensions 
     # This method returns a 33x33 .png of the code
-    def to_img(bg_color = ChunkyPNG::Color::WHITE)
-      return Image.new(self).render(bg_color)
+    def to_img(bg_color = ChunkyPNG::Color::TRANSPARENT, fg_color = ChunkyPNG::Color::BLACK)
+      return Image.new(self).render(bg_color, fg_color)
+    end
+    
+    def to_img_rgba(bg_color = [0,0,0,0], fg_color = [0,0,0,255])
+      chunky_bg = ChunkyPNG::Color.rgba(bg_color[0],bg_color[1],bg_color[2],bg_color[3])
+      chunky_fg = ChunkyPNG::Color.rgba(fg_color[0],fg_color[1],fg_color[2],fg_color[3])
+      return Image.new(self).render(chunky_bg, chunky_fg)
     end
   end
 end
